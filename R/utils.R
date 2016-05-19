55cfa4c0f49ee0ceba5e41b3680b0ac6e2640749
christophergandrud <christopher.gandrud@gmail.com>
2016-05-19 09:43:57 +0100

    me_one for cat = 0

--- R/utils.R
+++ R/utils.R
@@ -3,14 +3,22 @@
 #' @noRd
 
 me_one <- function(term1_, int_term_, fitted2_,
-                   beta_hat, cov, ci_)
+                   beta_hat, cov, ci_, term2_0 = FALSE, obj_)
 {
-    dy_dx <- beta_hat[term1_] + beta_hat[int_term_] * fitted2_
+    if (!isTRUE(term2_0)) {
+        dy_dx <- beta_hat[term1_] + beta_hat[int_term_] * fitted2_
 
-    # Standard error
-    se_dy_dx <- sqrt(cov[term1_, term1_] +
-                         fitted2_ ^ 2 * cov[int_term_, int_term_] +
-                         2 * fitted2_ *cov[term1_, int_term_])
+        # Standard error
+        se_dy_dx <- sqrt(cov[term1_, term1_] +
+                             fitted2_ ^ 2 * cov[int_term_, int_term_] +
+                             2 * fitted2_ *cov[term1_, int_term_])
+    }
+
+    else if (isTRUE(term2_0)) {
+        dy_dx <- beta_hat[term1_]
+        all_b_se <- coef(summary(obj_))[, 'Std. Error']
+        se_dy_dx <- all_b_se[term1_]
+    }
 
     # Confidence interval
     z <- qnorm(ci_ / 100)
